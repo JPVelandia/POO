d09811712823d82da35655a1abf12fc60cecde51
JuanchoBot98 <ezivalart@gmail.com>
2019-10-28 23:34:37 -0500

    versión ya casi

---
+++ Posible código menú principal/New Unity Project/Assets/control_menu.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class control_menu : MonoBehaviour
+{
+    public void Jugar()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void Regresar()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void Instrucciones()
+    {
+        SceneManager.LoadScene(2);
+    }
+
+    public void Creditos()
+    {
+        SceneManager.LoadScene(3);
+    }
+
+    public void Salir()
+    {
+        Debug.Log("Se ha cerrado el juego");
+        Application.Quit();
+    }
+}
